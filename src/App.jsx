214a687437342870faaec199597f075edbb3573e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-27 22:50:33 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Intro from './Components/Intro/Intro'
+import Skills from './Components/Skills/Skills'
+import Works from './Components/Works/Works'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='app'>
-      Hello
+      <>
+      <Navbar/>
+      <Intro/>
+      <Skills/>
+      <Works/>
+      <Contact/>
+      <Footer/>
+      </>
     </div>
   )
 }
