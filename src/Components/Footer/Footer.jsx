639b28dcc8856516392e78e650b94595238ad2b6
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-27 22:52:24 +0100

    commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy; 2024, Akinrinola Muiz A. All right reserved.
+    </div>
+  )
+}
+
+export default Footer
