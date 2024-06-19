735feb10075dc711aad06a9e5748f606b7975c6f
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-19 23:42:41 +0100

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/tech logo.png";
 import contact_icon from "../../assets/contact.png";
 import { Link } from "react-scroll";
-import menu from '../../assets/menu.png'
+import menu from '../../assets/bars.png'
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false)
