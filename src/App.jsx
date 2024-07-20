61ee330761d7b28a3c6c2f5111c970980fb64b9e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-07-20 11:16:47 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import './App.css'
 import Navbar from "./Components/Navbar/Navbar";
 import Intro from "./Components/Intro/Intro";
 import Skills from "./Components/Skills/Skills";
 import Works from "./Components/Works/Works";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import video from "./assets/video-2.mp4"
 
 const App = () => {
   return (
     
     <div className="app">
       <>
-      {/* <video src="./assets/video-2.mp4">Hello</video> */}
+      <video autoPlay loop muted className="bg-vid">
+    <source src={video} type="video/mp4" />
+  </video>
         <Navbar />
         <Intro />
         
         <Skills />
+        
         <Works />
         <Contact />
         <Footer />
